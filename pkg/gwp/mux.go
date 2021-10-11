5a860f03f1b51564a21adba558793bbf1ef212e9
Bjørn Borud <borud@lab5e.com>
2021-10-11 23:02:28 +0200

    added comments and added seal-check

--- pkg/gwp/mux.go
+++ pkg/gwp/mux.go
@@ -2,19 +2,31 @@ package gwp
 
 import (
 	"errors"
+	"log"
 
 	"github.com/borud/gwp/pkg/gwpb"
 	"google.golang.org/protobuf/proto"
 )
 
+// Mux is a multiplexing Handler.
 type Mux struct {
 	sealed   AtomicBool
 	handlers map[string]Handler
 }
 
 var (
+	// ErrHandlerAlreadyDefined indicates that you tried to set a handler
+	// that has already been set.
 	ErrHandlerAlreadyDefined = errors.New("handler already added")
-	ErrCannotResolveName     = errors.New("cannot resolve name of message type")
+
+	// ErrCannotResolveName means that the payload type you tried to add a handler
+	// for is not a known payload type.
+	ErrCannotResolveName = errors.New("cannot resolve name of message type")
+
+	// ErrMuxSealed is returned when you try to add a handler after the
+	// mux has been sealed.  The mux is sealed when you explicitly call Seal()
+	// or when you.
+	ErrMuxSealed = errors.New("you cannot change a sealed mux")
 )
 
 // NewMux creates a new Mux instance.  You can add handlers to it
@@ -26,6 +38,7 @@ func NewMux() *Mux {
 	}
 }
 
+// Handle a request
 func (m *Mux) Handle(r Request) {
 	name := payloadTypeName(r.Packet)
 	if name == "" {
@@ -35,7 +48,10 @@ func (m *Mux) Handle(r Request) {
 	h, ok := m.handlers[name]
 	if ok {
 		h(r)
+		return
 	}
+	// TODO(borud): remove this when we have a better way of handling requests.
+	log.Printf("Mux: unhandled request: %+v", r)
 }
 
 // Seal ensures that the Mux cannot be changed.
@@ -45,6 +61,10 @@ func (m *Mux) Seal() {
 
 // AddHandler adds a handler for a given payload type.
 func (m *Mux) AddHandler(payloadType proto.Message, handler Handler) error {
+	if m.sealed.IsTrue() {
+		return ErrMuxSealed
+	}
+
 	name := typeName(payloadType)
 	if name == "" {
 		return ErrCannotResolveName
